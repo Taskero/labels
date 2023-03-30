e921dbb17745bc019dffa25e497acb46ccdeb472
Matias Berrueta <matiasberrueta@gmail.com>
2023-03-30 19:46:22 -0300

    Labels server

--- lib/labels.ex
+++ lib/labels.ex
@@ -1,18 +1,60 @@
 defmodule Labels do
   @moduledoc """
-  Documentation for `Labels`.
+  Labels is a server to store and retrieve labels for a given key.
+  It avoid duplicates and is case insensitive.
   """
+  use GenServer
+
+  # Client
 
   @doc """
-  Hello world.
+  Starts the server.
 
   ## Examples
-
-      iex> Labels.hello()
-      :world
-
+    {:ok, pid} = Labels.start_link()
+    {:ok, #PID<0.173.0>}
   """
-  def hello do
-    :world
+  @spec start_link(keyword()) :: {:ok, pid}
+  def start_link(args \\ []), do: GenServer.start_link(__MODULE__, args, name: __MODULE__)
+
+  @doc """
+  Returns the list of unique labels by request service.
+  """
+  def get_all(service), do: GenServer.call(__MODULE__, {:get_all, service})
+
+  @doc """
+  Add a new one
+  """
+  def add(service, label), do: GenServer.cast(__MODULE__, {:add, service, label})
+
+  # Server
+
+  def init(_args) do
+    table = :ets.new(:labels, [:ordered_set, :public])
+
+    # TODO: load from disk
+
+    {:ok, table}
   end
+
+  def handle_call({:get_all, service}, _from, table) do
+    case :ets.lookup(table, service) do
+      [{_, labels}] -> {:reply, labels, table}
+      [] -> {:reply, [], table}
+    end
+  end
+
+  def handle_cast({:add, service, label}, table) do
+    case :ets.lookup(table, service) do
+      [{_, labels}] ->
+        :ets.insert(table, {service, [label |> clean() | labels] |> Enum.uniq() |> Enum.sort()})
+        {:noreply, table}
+
+      [] ->
+        :ets.insert(table, {service, [label]})
+        {:noreply, table}
+    end
+  end
+
+  defp clean(label), do: label |> String.downcase() |> String.trim() |> String.replace(" ", "_")
 end
--- test/labels_test.exs
+++ test/labels_test.exs
@@ -2,7 +2,30 @@ defmodule LabelsTest do
   use ExUnit.Case
   doctest Labels
 
-  test "greets the world" do
-    assert Labels.hello() == :world
+  test "init server without labels" do
+    assert {:ok, _pid} = Labels.start_link()
+
+    assert [] = Labels.get_all("test_service")
+  end
+
+  test "Add labels" do
+    assert {:ok, _pid} = Labels.start_link()
+    assert :ok = Labels.add("test_service", "test_label")
+    assert ["test_label"] = Labels.get_all("test_service")
+  end
+
+  test "Add labels case insensitive and avoid duplicates " do
+    assert {:ok, _pid} = Labels.start_link()
+    assert :ok = Labels.add("test_service", "test_label")
+    assert ["test_label"] = Labels.get_all("test_service")
+
+    assert :ok = Labels.add("test_service", "test_labeL_SECOND")
+    assert ["test_label", "test_label_second"] = Labels.get_all("test_service")
+
+    # avoid spaces
+    assert :ok = Labels.add("test_service", "  test_label  ")
+    assert ["test_label", "test_label_second"] = Labels.get_all("test_service")
+    assert :ok = Labels.add("test_service", "  test label  ")
+    assert ["test_label", "test_label_second"] = Labels.get_all("test_service")
   end
 end
